5f00697c921dba1f9d5a39d49b1ae8ba9950ced3
freezy <freezy@kodi.tv>
2023-03-28 09:03:06 +0200

    project: Update Android embedding.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -80,12 +80,11 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
+            OutlinedButton(
               onPressed: () {
                 _showNPS();
               },
               child: Text("Show NPS"),
-              color: Colors.grey,
             ),
           ],
         ),
